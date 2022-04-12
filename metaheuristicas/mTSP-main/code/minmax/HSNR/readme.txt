b7587426d711a1924ee71c0e81be23e0eaf374a3
vitor martini <vitor-martini@outlook.com>
2022-04-11 21:52:51 -0300

    Finalizado importação de instâncias e consulta de distâncias euclideanas

--- metaheuristicas/mTSP-main/code/minmax/HSNR/readme.txt
+++
@@ -1,1 +0,0 @@
-this code includes that all edges from elite solutions are added into the edge.
--- metaheuristicas/mTSP.py
+++ metaheuristicas/mTSP.py
@@ -1,43 +1,78 @@
 import os
+import math
 
 class Instance:
     
-    def __init__(self, name, x, y):
+    def __init__(self, name, points):
         self.name = name
-        self.x = x
-        self.y = y
+        self.points = points
+        self.matrix = generate_matrix(points)
+
+def generate_matrix(points):
+    matrix = []
+    for i in range(len(points)): 
+        row = [] 
+        for j in range(len(points)):
+            row.append(math.dist(points[i], points[j])) 
+        matrix.append(row)  
+
+    return matrix  
+
+def search_instance(list_of_instance, name):
+    for i in range(len(list_of_instance)): 
+        if list_of_instance[i].name == name:
+            return i
+
+    return -1
+
+def print_instance(list_of_instance, option):
+    index = search_instance(list_of_instance, option)
+    if index == -1:
+        print('File not found.')
+        return
+    else:
+        with open(os.path.dirname(os.path.abspath(__file__)) + '\\results_' + list_of_instance[index].name +'.txt', 'w+') as list_file:
+
+            for i in range(len(list_of_instance[index].points)): 
+                list_file.write(str(i+1) + ' ' + str(list_of_instance[index].points[i]) + '\n')
+
+            for row in list_of_instance[index].matrix:                
+                list_file.write(str(row))
+                list_file.write('\n')
+
+def treat_character(line):
+    line = line.replace('\n', '').replace('	', ' ').split(' ')
+    return line[0].isnumeric(), line 
 
 def import_instances():
     walk_dir = os.path.dirname(os.path.abspath(__file__)) + '\instances'
-
     list_of_instance = []
 
-    for root, subdirs, files in os.walk(walk_dir):
-        
-        for filename in files:
+    for root, subdirs, files in os.walk(walk_dir):        
+        for filename in files:            
             instance_file = open(os.path.join(root, filename), 'r')
             i = 0
-            x = []       
-            y = [] 
-            for linha in instance_file: 
+            points = []   
+            for line in instance_file: 
+                xy = []  
+                validation, line = treat_character(line)
                 if i == 0:
-                    name = linha.replace('\n', '').split(' ')[0] + '_' +linha.replace('\n', '').split(' ')[2]
+                    name = line[0] + '_' + line[2]
                     i+=1
-                else:
-                    if linha != '\n':
-                        x.append(linha.replace('\n', '').split(' ')[1])
-                        y.append(linha.replace('\n', '').split(' ')[2])
+                else:                    
+                    if validation == True:
+                        xy.append(float(line[1]))
+                        xy.append(float(line[2]))
+                        points.append(xy)
 
-            list_of_instance.append(Instance(name, x, y))
-
+            list_of_instance.append(Instance(name, points))
     return list_of_instance
      
 list_of_instance = import_instances()
-for item in list_of_instance:
-    print(item.name)
-    print('x: ')
-    print(item.x)
-    print('y: ')
-    print(item.y)
-    print('\n')
-    
+
+while(True):
+    option = input('Filename: ')
+    if option == 'exit':
+        break
+    else:
+        print_instance(list_of_instance, option)
