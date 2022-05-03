28a9fde2f6ea26d3d1d25f9c0dce459c9908953e
vitor martini <vitor-martini@outlook.com>
2022-05-03 12:30:54 -0300

    03/05/2022

--- metaheuristicas/mTSP/algorithm/grasp.py
+++ metaheuristicas/mTSP/algorithm/grasp.py
@@ -1,5 +1,6 @@
-from random import sample
+import random 
 import copy
+import local_search
 
 def generate_candidates(instance):
     candidates = copy.deepcopy(instance.points)
@@ -9,10 +10,25 @@ def generate_candidates(instance):
 def sort_by_fo(e):
   return e['cost']
 
-def create_routes(instance):    
-    routes = [[0] for item in range(instance.vehicles_quantity)] # Cria uma lista de listas baseado na quantidade de veículos
-    candidates = generate_candidates(instance) # Gera a lista de candidatos
+def get_random_candidate(RCL, RLC_length_in_percentage, alpha):
+    RCL.sort(key=sort_by_fo)     
+    best_candidate_values = RCL[0]
+    best_candidate_cost = best_candidate_values['cost']
+    worst_candidate_values = RCL[len(RCL) - 1]
+    worst_candidate_cost = worst_candidate_values['cost']
+    split_index = int(len(RCL) * RLC_length_in_percentage / 100)
+    RCL = RCL[:split_index] # Dimunui o tamanho da lista para apenas (RLC_length_in_percentage)%
+    condition = best_candidate_cost + alpha * (worst_candidate_cost - best_candidate_cost)
 
+    for item in RCL:
+        if (item['cost'] <= condition) == False:
+            RCL.remove(item)
+
+    return random.choice(RCL)
+   
+def semi_greedy_construction(instance, RLC_length_in_percentage, alpha):    
+    routes = [[instance.points[0]] for item in range(instance.vehicles_quantity)] # Cria uma lista de listas baseado na quantidade de veículos
+    candidates = generate_candidates(instance) # Gera a lista de candidatos
     fo = 0
     RCL = []
     while len(candidates) > 0: # Enquanto houver candidatos
@@ -20,48 +36,54 @@ def create_routes(instance):
             for route_index in range(len(routes)): # Para cada rota
                 route_length = len(routes[route_index])
                 for point_index in range(route_length): # Para cada ponto da rota
-                    candidate_index = instance.points.index(candidate) # Seleciona o índice do candidato na lista de pontos
+                    candidate_index = candidate['index'] # Seleciona o índice do candidato na lista de pontos
 
-                    # Calcula o custo do candidato
-                    cost = instance.matrix[routes[route_index][point_index]][candidate_index]
+                    cost = instance.matrix[routes[route_index][point_index]['index']][candidate_index] # Custo do ponto até o candidato
 
-                    if point_index == route_length - 1:
-                        cost += instance.matrix[candidate_index][0] 
-                        cost -= instance.matrix[routes[route_index][point_index]][0]                   
+                    if point_index == route_length - 1: # Se o ponto selecionado é o último da lista
+                        cost += instance.matrix[candidate_index][0] # + Distância do candidato a garagem
+                        cost -= instance.matrix[routes[route_index][point_index]['index']][0] # - Distância do ponto corrente a garagem         
                     else:
-                        cost += instance.matrix[candidate_index][routes[route_index][point_index + 1]] 
-                        cost -= instance.matrix[routes[route_index][point_index]][routes[route_index][point_index + 1]]
+                        cost += instance.matrix[candidate_index][routes[route_index][point_index + 1]['index']] # + Distância do candidato ao próximo ponto
+                        cost -= instance.matrix[routes[route_index][point_index]['index']][routes[route_index][point_index + 1]['index']]# - Distância do ponto corrente ao próximo ponto
                     
                     # Insere os dados na lista
-                    element = {}
-                    element['cost'] = cost
-                    element['candidate'] = candidate
-                    element['route_index'] = route_index
-                    element['point_index'] = point_index
-                    RCL.append(element)
+                    candidate_values = {}
+                    candidate_values['cost'] = cost
+                    candidate_values['candidate'] = candidate
+                    candidate_values['route_index'] = route_index
+                    candidate_values['point_index'] = point_index
+                    RCL.append(candidate_values)
             
-        RCL.sort(key=sort_by_fo)     
-        best_candidate_values = RCL[0]
-
-        best_candidate_cost = best_candidate_values['cost']
-        best_candidate_point = best_candidate_values['candidate']
-        best_candidate_point_index = instance.points.index(best_candidate_point)
-        best_candidate_route = best_candidate_values['route_index']
-        best_candidate_new_index = best_candidate_values['point_index'] + 1
-        
-        routes[best_candidate_route].insert(best_candidate_new_index, best_candidate_point_index)
-        fo += best_candidate_cost
-        candidates.remove(best_candidate_point)
+        selected_candidate_values = get_random_candidate(RCL, RLC_length_in_percentage, alpha) # Seleciona candidato de forma parcialmente randômica
+        selected_candidate = selected_candidate_values['candidate']
+        selected_candidate_route_cost = selected_candidate_values['cost']
+        selected_candidate_route = selected_candidate_values['route_index']
+        selected_candidate_new_index = selected_candidate_values['point_index'] + 1
+        fo += selected_candidate_route_cost
+        routes[selected_candidate_route].insert(selected_candidate_new_index, selected_candidate) # Adiciona candidato na solução    
+           
+        candidates.remove(selected_candidate) # Remove da lista de candidatos
         RCL.clear()
 
-    
     instance.add_best_solution(fo, routes)
-    instance.print_solutions()
     return routes
     
-def GRASP(instance):
-    route = create_routes(instance)
-    return route
+def GRASP(instance, GRASP_max, RLC_length_in_percentage, alpha):
+    fo_best = -1
+    for iteration in range(GRASP_max):
+        S = semi_greedy_construction(instance, RLC_length_in_percentage, alpha)
+        S = local_search.local_search(instance, S)
+
+        fo_current = instance.calculate_FO(S)
+        if  fo_current < fo_best or fo_best < 0:
+            S_best = copy.deepcopy(S)
+            fo_best = fo_current
+            instance.add_best_solution(fo_best, S_best)
+
+        print(iteration, fo_current, fo_best)
+
+    return S_best
 
     
 
--- metaheuristicas/mTSP/algorithm/instances.py
+++ metaheuristicas/mTSP/algorithm/instances.py
@@ -1,6 +1,5 @@
 import os
 import math
-import construction
 
 class Instance:    
     def __init__(self, name, points, vehicles_quantity):
@@ -10,24 +9,24 @@ class Instance:
         self.matrix = generate_matrix(points)
         self.fo_per_route = [0 for item in range(vehicles_quantity)] 
         self.best_fos = []
-        self.best_solutions = []
+        self.best_solutions = [] # Guarda as 100 melhores soluções
 
     def add_best_solution(self, fo, solution):
-        if len(self.best_fos) == 100:
+        if len(self.best_fos) == 100:  # Se a lista está lotada
             worst_value = max(self.best_fos)
-            if fo < worst_value:
-                index = self.best_fos.index(worst_value)
-                self.best_fos[index] = fo
+            if fo < worst_value: # E o FO encontrado é melhor que o pior FO da lista
+                index = self.best_fos.index(worst_value) # Seleciona o index do pior FO
+                self.best_fos[index] = fo # Adiciona o FO novo na lista, no lugar do antigo
                 self.best_solutions[index] = solution
-                self.calculate_fo_per_route(solution)
-        else:
+                self.update_fo_per_route(solution)
+        else: # Se a lista ainda não está lotada, insere o FO na lista
             self.best_fos.append(fo)
             self.best_solutions.append(solution)
-            self.calculate_fo_per_route(solution)
+            self.update_fo_per_route(solution)
 
-    def calculate_fo_per_route(self, solution):
+    def update_fo_per_route(self, solution): # Atualiza o FO de cada rota
         for route_index in range(len(solution) - 1):
-            self.fo_per_route[route_index] = construction.calculate_fo_per_route(self, solution[route_index])
+            self.fo_per_route[route_index] = self.calculate_fo_per_route(solution[route_index])
 
     def best_solution(self):
         best_fo = min(self.best_fos)
@@ -36,45 +35,71 @@ class Instance:
 
         return best_fo, best_solution
 
-    def all_solutions(self):
-        return self.best_solutions
+    def print_solution(self):
+        fo, routes = self.best_solution()
+        fo_manually = 0
+        print('WINNER ROUTE: ')
+        for i in range(len(routes)):
+            print('\n', end = '')
+            print('route ' + str(i+1) + ': ')
+            for item in routes[i]:
+                print(item['index'], end = ' ')
+            print('\n', end = '')
+            for item in routes[i]:
+                print(item['xy'], end = ' ')
+        
+            fo_route = self.calculate_fo_per_route(routes[i])
+            fo_manually += fo_route
+            print('\nfo manually calculated: ' + str(fo_route))
 
-    def print_solutions(self):
-        # print('BEST SOLUTIONS:')
-        # for index in range(len(self.best_solutions)):
-        #     print(str(index + 1) + ' -> ', end = '')
-        #     print(self.best_solutions[index])
-
-        # print('================================================================================')
-
-        fo, route = self.best_solution()
-        print('WINNER:')                    
-        print('ROUTES: ' + str(route))
+        print('\nfo manually calculated: ' + str(fo_manually))
         print('FO: ' + str(fo))
 
+    def calculate_FO(self, routes):
+        fo = 0
+        for route in routes: 
+            fo += self.calculate_fo_per_route(route)
+            
+        return fo
+
+    def calculate_fo_per_route(self, route):
+        fo = 0
+        if len(route) > 1:     
+            first_point_index = route[1]['index']
+            fo += self.matrix[0][first_point_index] # Distância da garagem ao primeiro ponto da rota
+            for point_index in range(len(route)): # Para cada ponto
+                point = route[point_index]['index']
+                if point_index > 0 and point_index < len(route) - 1: # Se o index do ponto não for a garagem nem o último ponto                    
+                    next_point = route[point_index + 1]['index'] 
+                    fo += self.matrix[point][next_point] # Distância entre o ponto e o próximo ponto
+                elif point_index == len(route) - 1: # Se for o último ponto da rota
+                    fo += self.matrix[point][0] # Distância entre o ponto e a garagem
+        
+        return fo
+        
 def generate_matrix(points):
     matrix = []
-    for i in range(len(points)): 
+    for i in range(len(points)): # Para cada ponto
         row = [] 
         for j in range(len(points)):
-            row.append(math.dist(points[i], points[j])) 
+            row.append(math.dist(points[i]['xy'], points[j]['xy'])) # Calcula a distância dele para todos os outros pontos
         matrix.append(row)  
 
     return matrix  
 
-def print_instance(list_of_instance):
+def export_instance(list_of_instance): # Exporta a instância para arquivo txt
     for index in range(len(list_of_instance)):        
         with open(os.path.dirname(os.path.abspath(__file__)) + '\\results_' + list_of_instance[index].name +'.txt', 'w+') as list_file:
-            list_file.write('vehicles_quantity: ' + str(list_of_instance[index].vehicles_quantity) + '\n')
+            list_file.write('vehicles_quantity: ' + str(list_of_instance[index].vehicles_quantity) + '\n') # Quantidade de veículos
             
             for i in range(len(list_of_instance[index].points)): 
-                list_file.write(str(i+1) + ' ' + str(list_of_instance[index].points[i]) + '\n')
+                list_file.write(str(list_of_instance[index].points[i]['index']) + ' ' + str(list_of_instance[index].points[i]['xy']) + '\n') # Pontos
 
-            for row in list_of_instance[index].matrix:                
+            for row in list_of_instance[index].matrix: # Matriz de distância
                 list_file.write(str(row))
                 list_file.write('\n')
-
-def treat_character(line):
+      
+def treat_character(line): 
     line = line.replace('\n', '').replace('	', ' ').replace('  ', ' ').split(' ')
     return line[0].isnumeric(), line 
 
@@ -83,22 +108,28 @@ def import_instances():
     list_of_instance = []
 
     for root, subdirs, files in os.walk(walk_dir):        
-        for filename in files:            
+        for filename in files: # Para cada arquivo de instância           
             instance_file = open(os.path.join(root, filename), 'r')
-            i = 0
-            points = []   
-            for line in instance_file: 
+            first_line = True 
+            point_index = 0
+            points = []  
+            for line in instance_file: # Para cada linha do arquivo                
                 xy = []  
                 validation, line = treat_character(line)
-                if i == 0:
+                if first_line == True: # Se for a primeira linha, quarda a qtd de veículos e o nome da instância
                     vehicles_quantity = int(line[2])
                     name = line[0] + '_' + str(vehicles_quantity)
-                    i+=1
-                else:                    
-                    if validation == True:
+                    first_line = False
+                else: # Caso contrário, guarda o index do ponto (para usar na matriz de distância) e o ponto
+                    if validation == True:                        
                         xy.append(float(line[1]))
                         xy.append(float(line[2]))
-                        points.append(xy)
+                        element = {
+                            'index': point_index,
+                            'xy': xy
+                        }
+                        points.append(element)
+                        point_index+=1
 
             list_of_instance.append(Instance(name, points, vehicles_quantity))
     return list_of_instance
