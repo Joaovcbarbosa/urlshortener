e3c2c84bc1337ff7cad254ef14bc1fdcb9ffc9b8
vitor martini <vitor-martini@outlook.com>
2022-05-07 14:41:23 -0300

    07/05/2022

--- metaheuristicas/mTSP/algorithm/test_solutions.py
+++ metaheuristicas/mTSP/algorithm/test_solutions.py
@@ -6,5 +6,5 @@ def test_local_search():
     instance = instances.generate_test_instance()
     construction.create_test_routes(instance)
     local_search.local_search(instance) 
-    assert instance.current_solution_fo == 515.7965443724249
+    assert float("{0:.4f}".format(instance.current_solution_fo)) == 515.7965
 
