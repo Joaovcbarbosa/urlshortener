4fde80f93224c32ccb54cf580227174fd929960e
Vitor Martini <vitor-martini@outlook.com>
2022-05-16 16:57:46 -0300

    cada rota deve ter pelo menos um ponto

--- metaheuristicas/mTSP/algorithm/main.py
+++ metaheuristicas/mTSP/algorithm/main.py
@@ -19,7 +19,7 @@ def main():
     option = input(input_string)
     
     list_of_instance = import_instances()    
-    choice = 0
+    choice = None
     # export_instance(list_of_instance)
     
     for instance in list_of_instance:
