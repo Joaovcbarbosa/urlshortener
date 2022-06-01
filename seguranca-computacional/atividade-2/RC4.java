a5020b8bd517f94a3bf21720fac48948fa53804d
vitor martini <vitor-martini@outlook.com>
2022-06-01 09:19:50 -0300

    rc4

--- seguranca-computacional/atividade-2/RC4.java
+++ seguranca-computacional/atividade-2/RC4.java
@@ -1,44 +1,60 @@
 import java.io.*;
 
 public class RC4 {
-    private byte[] S = new byte[256];
-    private byte[] T = new byte[256];
+    public byte[] S = new byte[256];
+    public byte[] T = new byte[256];
+    public byte[] key = new byte[256];
 
-    public RC4(byte[] key) {        
-        int i, j;
-        byte tmp;
+    public RC4(byte[] key) {  
+        this.key = key;
+        SInitialization();
+        SPermutation();
+    }
+
+    public void SInitialization(){
+        int i;
+
         for (i = 0; i < 256; i++) {
             S[i] = (byte) i;
             T[i] = key[i % key.length];
         }
+    }
+
+    public void SPermutation(){
+        int i, j;    
+
         j = 0;
         for (i = 0; i < 256; i++) {
             j = (j + S[i] + T[i]) & 0xFF;
-            tmp = S[j];
-            S[j] = S[i];
-            S[i] = tmp;
+            swap(i, j);
         }
     }
 
-    public byte[] encrypt(byte[] plaintext) {
-        byte[] ciphertext = new byte[plaintext.length];
+    public byte[] encrypt(byte[] plainText) {
         int i = 0, j = 0, k, t;
-        byte tmp;
-        for (int counter = 0; counter < plaintext.length; counter++) {
+        byte[] encryptedTextBytes = new byte[plainText.length];
+
+        for (k = 0; k < plainText.length; k++) {
             i = (i + 1) & 0xFF;
             j = (j + S[i]) & 0xFF;
-            tmp = S[j];
-            S[j] = S[i];
-            S[i] = tmp;
+            swap(i, j);
             t = (S[i] + S[j]) & 0xFF;
-            k = S[t];
-            ciphertext[counter] = (byte) (plaintext[counter] ^ k);
+            encryptedTextBytes[k] = (byte) (plainText[k] ^ S[t]);
         }
-        return ciphertext;
+        return encryptedTextBytes;
+    }
+
+    public void swap(int i, int j){        
+        byte aux;
+
+        aux = S[j];
+        S[j] = S[i];
+        S[i] = aux;
     }
 
     public String byteToHexadecimal(byte[] bytes){
         StringBuilder oStringBuilder = new StringBuilder();
+
         for (byte b : bytes) {
             oStringBuilder.append(String.format("%02X:", b));
         }
