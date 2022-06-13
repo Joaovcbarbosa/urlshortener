f088b257dabe1301698d036a250e60f2366578da
vitor martini <vitor-martini@outlook.com>
2022-06-13 16:35:05 -0300

    novas tentativas

--- teoria-dos-grafos/atividade-2/atividade-2.py
+++ teoria-dos-grafos/atividade-2/atividade-2.py
@@ -11,46 +11,20 @@ class Grafo:
         self.grafo[u].append(v)
         self.grafo[v].append(u)
  
-    def eh_vertice_de_corte(self, u, vertices_visitados, pai):
-        vertices_visitados[u] = True
-        filho = 0
-        for v in self.grafo[u]:
-            if vertices_visitados[v] == False :
-                pai[v] = u
-                filho += 1
-                self.eh_vertice_de_corte(v, vertices_visitados, pai)
-                if pai[u] == -1 and filho > 1:
-                    return True
- 
-        return False 
- 
-    def vertice_de_corte(self):
-        # vertices_visitados = [False] * (self.qtd_vertices)
-        # pai = [-1] * (self.qtd_vertices)
- 
-        # i = self.qtd_vertices - 1
- 
-        # if len(self.grafo[i]) >= 2:
-        #     return self.calcular_soma_maior_regiao()
- 
-        # if self.eh_vertice_de_corte(self.qtd_vertices - 1, vertices_visitados, pai) == False:
-        #     return i + 1
-        
-        return self.calcular_soma_maior_regiao()
- 
     def calcular_soma_maior_regiao(self):
         raizes = []
         sum_ids = 0
         max_id = 0
-        for v in self.grafo[self.qtd_vertices - 1]:
-            self.grafo[v].remove(self.qtd_vertices - 1)
-            raizes.append(v)
- 
-        for u in raizes:
-            value = self.DFS(u)
-            sum_ids += value
-            if value > max_id:
-                max_id = value
+        if len(self.grafo[self.qtd_vertices - 1]) >= 2:
+            for v in self.grafo[self.qtd_vertices - 1]:
+                self.grafo[v].remove(self.qtd_vertices - 1)
+                raizes.append(v)
+    
+            for u in raizes:
+                value = self.DFS(u)
+                sum_ids += value
+                if value > max_id:
+                    max_id = value
  
         return sum_ids - max_id + self.qtd_vertices
  
@@ -82,7 +56,7 @@ def main():
         v = int(v) - 1
         g.adicionar_aresta(u, v)
  
-    print(g.vertice_de_corte())
+    print(g.calcular_soma_maior_regiao())
     
  
 main()
