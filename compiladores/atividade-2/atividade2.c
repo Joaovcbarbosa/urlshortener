d696245c0598669142203a8a800936bea7e89f66
Vitor Martini <vitor-martini@outlook.com>
2022-09-25 15:19:26 -0300

    fix: ex 2 concluido

--- compiladores/atividade-2/atividade2.c
+++ compiladores/atividade-2/atividade2.c
@@ -1,10 +1,9 @@
-// C program to illustrate the regexec() function
 #include <regex.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
 #include <stdio.h>
- 
+
 void toUpperCase(char *wordBuffer)
 {
     int j = 0;
@@ -13,52 +12,38 @@ void toUpperCase(char *wordBuffer)
         wordBuffer[j] = toupper(wordBuffer[j]);
         j++;
     }
-	printf("%s ", wordBuffer);
 }
 
-void tratarCaracter(char *wordBuffer){
-	regex_t reegex;
+void tratarCaracter(char *wordBuffer){	
 	int value, pattern;
+	regex_t reegex;
 
-	pattern = regcomp(&reegex, "if|else|void|int|return", 0); 
+	pattern = regcomp(&reegex, "if|else|void|int|return", REG_EXTENDED|REG_NOSUB);
 	value = regexec( &reegex, wordBuffer, 0, NULL, 0);	
-	if(value == 0){
-		toUpperCase(wordBuffer);
-	}
+	
+	if(value == 0)
+		toUpperCase(wordBuffer);	
 }
 
-// Driver Code
 int main()
 {
 	FILE *sourceFile;
     FILE *targetFile;
-	regex_t reegex;
+	
 
     char ch;
     sourceFile = fopen("gcd.txt", "r");
     targetFile = fopen("saida.txt", "w");
 	char *wordBuffer = (char*)malloc(sizeof(char));
 	int wordBufferLen = 0;
-	int result = 1;
-
+	
     while((ch = fgetc(sourceFile)) != EOF){
-		if (ch == ')' || ch == '(' || ch == ',' || ch == ';' || isspace(ch) || ch == ' '){
+		if (ch == ')' || ch == '(' || ch == ',' || ch == ';' || isspace(ch) || ch == ' ' || ch == '\n' || ch == '\t'){
 			if(wordBufferLen > 0){
-				wordBuffer[wordBufferLen] = '\0';
 				
-				result = regexec( &reegex, wordBuffer, 0, NULL, 0);	
-				printf("%i", result);
-				if(result == 0){
-					printf("%s\n", wordBuffer);
-					toUpperCase(wordBuffer);
-				}
-// tratarCaracter(wordBuffer);	
-			 	// 
-
-			}	
-			 
-			
-		
+				tratarCaracter(wordBuffer);	
+				wordBuffer[wordBufferLen] = '\0';
+			}
 			fputs(wordBuffer, targetFile);
 			fputc(ch, targetFile);
 			wordBuffer = NULL;
