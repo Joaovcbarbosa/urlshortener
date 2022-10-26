305b51d204f5d49d84b7df6552a695274f8f5efa
Vitor Martini <vitor.martini@inovan.do>
2022-10-26 17:24:14 -0300

    atividade-2

--- compiladores/atividade-2/A.c
+++ compiladores/atividade-2/A.c
@@ -7,6 +7,7 @@
 int laco = 1;
 int estado = 0;
 int novoEstado = 0;
+int comentario = 0;
 
 int aceitacao[3] = {0, 0, 1};
 int transicao[3][3] = {
@@ -19,6 +20,37 @@ int avanco[3][3] = {
 						{1, 1, 0},
 						{0, 0, 0}
 				   };
+
+char palavraReservada[7][255] = {{"int"}, {"while"}, {"if"}, {"return"}, {"void"}, {"input"}, {"output"}};
+
+char avancarCaracter(FILE *arquivo)
+{
+	char c = getc(arquivo);
+	return c == EOF ? EOF : c;
+}
+
+char verificarProximoCaracter(FILE *arquivo)
+{
+	char c = getc(arquivo);
+	return c == EOF ? EOF : ungetc(c, arquivo);
+}
+
+int ehPalavraReservada(char *bufferPalavra){
+	int i;
+    for(i = 0; i < 7; i++) {
+        if(strcmp(bufferPalavra, palavraReservada[i]) == 0)
+			return 1;
+    }
+
+	return 0;
+}	
+
+int ehIdentificador(char *bufferPalavra){	
+	if(ehPalavraReservada(bufferPalavra) == 1)
+		return 0;
+
+	return 1;
+}	
 
 int ehLetra(int c){
 	if((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
@@ -41,6 +73,20 @@ int ehOutro(int c){
 	return 0;
 }	
 
+int ehComentario(int c, FILE *arquivo){
+	if (c == 47 && verificarProximoCaracter(arquivo) == 42)
+		return 1;
+
+	return 0;
+}	
+
+int ehFimComentario(int c, FILE *arquivo){
+	if (c == 42 && verificarProximoCaracter(arquivo) == 47)
+		return 1;
+
+	return 0;
+}	
+
 int grupo(int c){
 	if (ehLetra(c) == 1) return 0;
 	if (ehDigito(c) == 1) return 1;
@@ -49,23 +95,11 @@ int grupo(int c){
 	return -1;
 }
 
-char avancarCaracter(FILE *arquivo)
-{
-	char c = getc(arquivo);
-	return c == EOF ? EOF : c;
-}
-
-char verificarProximoCaracter(FILE *arquivo)
-{
-	char c = getc(arquivo);
-	return c == EOF ? EOF : ungetc(c, arquivo);
-}
-
 int main()
 {
 	FILE *arquivoFonte = fopen("sort.txt", "r");
     FILE *arquivoDestino = fopen("saida.txt", "w");
-	char c = "";
+	char c;
 
 	while(c != EOF){
 		char *bufferPalavra = '\0';
@@ -74,33 +108,34 @@ int main()
 		c = avancarCaracter(arquivoFonte);
 
 		if (ehLetra(c) == 0) {
+			if(ehComentario(c, arquivoFonte) == 1)
+				comentario = 1;
+			if(ehFimComentario(c, arquivoFonte) == 1)
+				comentario = 0;
+
 			fputc(c, arquivoDestino);
-			printf("%c\n", c);
 			continue; 
 		}
 		
 		while (aceitacao[estado] == 0){
 			novoEstado = transicao[estado][grupo(c)];
-			if (avanco[estado][grupo(c)] == 1) c = avancarCaracter(arquivoFonte);			
+			if (avanco[estado][grupo(c)] == 1){
+				tamanhoBuffer++;
+				bufferPalavra = (char*)realloc(bufferPalavra, (tamanhoBuffer) * (sizeof(char)));
+				bufferPalavra[tamanhoBuffer-1] = c;
+				bufferPalavra[tamanhoBuffer] = '\0';
+				c = avancarCaracter(arquivoFonte);	
+			} 		
 			estado = novoEstado;
-
-			tamanhoBuffer++;
-			bufferPalavra = (char*)realloc(bufferPalavra, (tamanhoBuffer) * (sizeof(char)));
-			bufferPalavra[tamanhoBuffer-1] = c;
-			bufferPalavra[tamanhoBuffer] = '\0';
-		}
+		}		
 		
-		fputs(bufferPalavra, arquivoDestino);
+		if(ehIdentificador(bufferPalavra) == 1 && comentario == 0)
+			fputs("ID", arquivoDestino);
+		else
+			fputs(bufferPalavra, arquivoDestino);
+
 		fputc(c, arquivoDestino);
-		printf("%c\n", bufferPalavra);
 		bufferPalavra = NULL;
 		tamanhoBuffer = 0;
-
-
-		
-
-
-	}
-	
-   
+	}	
 }
