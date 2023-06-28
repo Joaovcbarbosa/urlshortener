60854dcddc0a5a46c3c5d662469077689c8de3cf
João Barbosa <35549156+Joaovcbarbosa@users.noreply.github.com>
2023-06-28 11:11:05 -0300

    Update config.clj

--- programacao-funcional/url-shortener/src/url_shortener/config.clj
+++ programacao-funcional/url-shortener/src/url_shortener/config.clj
@@ -3,11 +3,19 @@
             [url-shortener.datomicadapter :as dt]))
 
 (defn
-  hash-url-and-save
+  hash-and-save-url
+  "Hashes the given URL and saves it to Datomic."
   [url]
-  (uc/hash-url-and-save dt/save-to-datomic url))
+  (try
+    (uc/hash-url-and-save dt/save-to-datomic url)
+    (catch Exception e
+      (println "An error occurred while hashing and saving the URL: " (.getMessage e)))))
 
 (defn
   get-original-url
+  "Retrieves the original URL from Datomic using the hashed URL."
   [url]
-  (uc/get-original-url dt/get-from-datomic url))
+  (try
+    (uc/get-original-url dt/get-from-datomic url)
+    (catch Exception e
+      (println "An error occurred while retrieving the original URL: " (.getMessage e)))))
